ff5fe91ffbafb155b61644eadc45d3375d6ecdd4
Felipe Oliveira <felipe.moraes@euemdia.com.br>
2019-12-16 07:07:17 -0300

    retirada imagem

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,11 +1,4 @@
-import Arrow from "./arrow.svg";
 import Bars from "./bars.svg";
-import CellPhone from "./cellphone.svg";
 import Close from "./close.svg";
-import Currency from "./currency.svg";
-import Email from "./email.svg";
-import Pencil from "./pencil.svg";
-import Profile from "./profile.svg";
-import Store from "./store.svg";
 
-export {Arrow, CellPhone, Close, Store, Currency, Email, Bars, Pencil, Profile};
+export {Close, Bars};
